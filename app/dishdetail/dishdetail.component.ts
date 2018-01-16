4271a6b1f67514b010a8b2fc5016aee19a9cc2c0
Paul Isaris <paulisaris@gmail.com>
2018-01-16 23:17:51 +0200

    Dialogs

--- app/dishdetail/dishdetail.component.ts
+++ app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,7 @@ import { RouterExtensions } from 'nativescript-angular/router';
 import 'rxjs/add/operator/switchMap';
 import {TNSFontIconService} from "nativescript-ngx-fonticon";
 import {FavoriteService} from "../services/favorite.service";
+import {Toasty} from "nativescript-toasty";
 
 @Component({
     selector: 'app-dishdetail',
@@ -50,8 +51,8 @@ export class DishdetailComponent implements OnInit {
         if (!this.favorite) {
             console.log('Adding to Favorites', this.dish.id);
             this.favorite = this.favoriteservice.addFavorite(this.dish.id);
-            // const toast = new Toast("Added Dish "+ this.dish.id, "short", "bottom");
-            // toast.show();
+             const toast = new Toasty("Added Dish "+ this.dish.id, "short", "bottom");
+             toast.show();
         }
     }
 
--- app/favorites/favorites.component.ts
+++ app/favorites/favorites.component.ts
@@ -6,6 +6,8 @@ import { RadListViewComponent } from 'nativescript-telerik-ui/listview/angular';
 import { ObservableArray } from 'tns-core-modules/data/observable-array';
 import { DrawerPage } from '../shared/drawer/drawer.page';
 import {View} from "tns-core-modules/ui/core/view";
+import {Toasty} from "nativescript-toasty";
+import { confirm } from "ui/dialogs";
 @Component({
     selector: 'app-favorites',
     moduleId: module.id,
@@ -32,9 +34,34 @@ export class FavoritesComponent extends DrawerPage implements OnInit {
     }
 
     deleteFavorite(id: number) {
-        this.favoriteservice.deleteFavorite(id)
-            .subscribe(favorites => this.favorites = new ObservableArray(favorites),
-                errmess => this.errMess = errmess);
+        console.log('delete', id);
+
+        let options = {
+            title: "Confirm Delete",
+            message: 'Do you want to delete Dish '+ id,
+            okButtonText: "Yes",
+            cancelButtonText: "No",
+            neutralButtonText: "Cancel"
+        };
+
+        confirm(options).then((result: boolean) => {
+            if(result) {
+
+                this.favorites = null;
+
+                this.favoriteservice.deleteFavorite(id)
+                    .subscribe(favorites => {
+                            const toast = new Toasty("Deleted Dish "+ id, "short", "bottom");
+                            toast.show();
+                            this.favorites = new ObservableArray(favorites);
+                        },
+                        errmess => this.errMess = errmess);
+            }
+            else {
+                console.log('Delete cancelled');
+            }
+        });
+
     }
 
     public onCellSwiping(args: ListViewEventData) {
