c22d2c3a1256107dabddfd3b6ef1cafc9746e3b8
Paul Isaris <paulisaris@gmail.com>
2018-01-29 18:55:37 +0200

    Application settings

--- app/shared/drawer/drawer.component.ts
+++ app/shared/drawer/drawer.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {TNSFontIconService} from "nativescript-ngx-fonticon";
-
+import { login, LoginResult } from "ui/dialogs";
+import { getString, setString } from "application-settings";
 @Component({
     selector: 'drawer-content',
     templateUrl: './shared/drawer/drawer.component.html',
@@ -8,5 +9,24 @@ import {TNSFontIconService} from "nativescript-ngx-fonticon";
 export class DrawerComponent {
 
     constructor(private fonticon: TNSFontIconService) {
+    }
+
+    displayLoginDialog() {
+        let options = {
+            title: "Login",
+            message: 'Type Your Login Credentials',
+            userName: getString("userName", ""),
+            password: getString("password",""),
+            okButtonText: "Login",
+            cancelButtonText: "Cancel"
+        };
+
+        login(options)
+            .then((loginResult: LoginResult) => {
+                    setString("userName", loginResult.userName);
+                    setString("password", loginResult.password);
+                },
+                () => { console.log('Login cancelled');
+                });
     }
 }
